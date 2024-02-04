47a0b75ced7fa4e76cbd4462ed3ca36ccc23b22e
Aran Hiblot <a.hiblot@student.isartdigital.com>
2024-02-04 15:54:37 +0100

    Fetch works

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const spreadsheetId = "1Ec9omsrDd4pRp5dTizj56Q3hCEpMoWh5ejAzlHaYI2Y";
-const range = "Players"; // Spécifiez la plage de cellules à lire
+let SHEET_ID = "1Ec9omsrDd4pRp5dTizj56Q3hCEpMoWh5ejAzlHaYI2Y";
+let SHEET_NAME = "Players";
 
-// Créez une requête HTTP GET pour récupérer les données
-const api = fetch(
-    `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}`
-)
-    .then((response) => response.json())
-    .then((data) => {
-        // Traitement des données récupérées
-        console.log("Données récupérées :", data);
-    })
-    .catch((error) => {
-        console.error("Erreur lors de la récupération des données :", error);
+let FULL_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?sheet=${SHEET_NAME}`;
+
+fetch(FULL_URL)
+    .then((res) => res.text())
+    .then((rep) => {
+        rep = JSON.parse(rep.substr(47).slice(0, -2));
+
+        rep.table.rows.slice(1).forEach((row) => {
+            let user = document.createElement("div");
+
+            let alliance = document.createElement("span");
+            alliance.innerHTML = row.c[0].v + " ";
+
+            let username = document.createElement("span");
+            username.innerHTML = row.c[1].v + " ";
+
+            let id = document.createElement("span");
+            id.innerHTML = row.c[2].v;
+
+            user.appendChild(alliance);
+            user.appendChild(username);
+            user.appendChild(id);
+
+            document.querySelector("body").appendChild(user);
+        });
     });
-
-console.log(api);
